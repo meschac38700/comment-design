14eba9cf9039bdb77b0023b483e65e7a9ee73723
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-09-12 13:25:18 +0200

    Get data from db or local

--- assets/js/utils/state.js
+++ assets/js/utils/state.js
@@ -1,58 +1,92 @@
-export default [
-	{
-		children: [],
-		parent: {
-			author__first_name: "Frederic",
-			author__username: "doefr",
-			nbr_vote: 0,
-			author__last_name: "Doe",
+const default_state = {
+	success: true,
+	previous: null,
+	next: null,
+	comments: [
+		{
+			id: 1,
 			added: "2020-07-15T00:00:00Z",
-			id: 7,
+			edited: "2021-09-11T18:20:56.879132+00:00",
 			content:
 				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam? Consectetur, dignissimos!",
+			top_parent_id: null,
+			user_id: 1,
+			parent_id: null,
+			votes: 10,
+			nb_children: 0,
+			owner_fullname: "Frederic doefr",
 		},
-	},
-	{
-		children: [
-			{
-				author__first_name: "Aymen",
-				author__username: "doeay",
-				nbr_vote: 0,
-				author__last_name: "Doe",
-				added: "2020-07-15T14:23:04Z",
-				id: 4,
-				content:
-					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam?",
-			},
-			{
-				author__first_name: "Jeremy",
-				author__username: "doeje",
-				nbr_vote: 0,
-				author__last_name: "Doe",
-				added: "2020-07-16T00:00:00Z",
-				id: 3,
-				content:
-					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam?",
-			},
-			{
-				author__first_name: "Thierry",
-				author__username: "doeth",
-				nbr_vote: 1,
-				author__last_name: "Doe",
-				added: "2020-07-16T00:00:00Z",
-				id: 8,
-				content: "Last comment on the first parent comment",
-			},
-		],
-		parent: {
-			author__first_name: "Eliam",
-			author__username: "doeel",
-			nbr_vote: 3,
-			author__last_name: "Doe",
-			added: "2020-07-15T14:20:46Z",
-			id: 1,
+		{
+			id: 2,
+			added: "2020-07-15T14:23:04Z",
+			edited: "2020-07-11T18:20:56.879132+00:00",
 			content:
-				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam? Consectetur, dignissimos! Officia debitis iste libero omnis porro facilis architecto! Officia corrupti cum vitae laborum minus exerci",
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam?",
+			top_parent_id: null,
+			user_id: 1,
+			parent_id: null,
+			votes: 10,
+			nb_children: 0,
+			owner_fullname: "Aymen doeay",
 		},
-	},
-];
+		{
+			id: 1,
+			added: "2020-07-16T00:00:00Z",
+			edited: "2020-07-11T18:20:56.879132+00:00",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam?",
+			top_parent_id: null,
+			user_id: 1,
+			parent_id: null,
+			votes: 10,
+			nb_children: 0,
+			owner_fullname: "Jeremy doeje",
+		},
+		{
+			id: 1,
+			added: "2020-07-16T00:00:00Z",
+			edited: "2020-07-11T18:20:56.879132+00:00",
+			content:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur ratione omnis alias magnam?",
+			top_parent_id: null,
+			user_id: 1,
+			parent_id: null,
+			votes: 10,
+			nb_children: 0,
+			owner_fullname: "Thierry doeth",
+			votes: 1,
+			author__last_name: "Doe",
+			added: "2020-07-16T00:00:00Z",
+			id: 8,
+			content: "Last comment on the first parent comment",
+		},
+	],
+};
+
+export let LOCAL_DATA = {
+	next: null,
+	previous: null,
+	comments: [],
+	success: false,
+};
+
+export const state = () => {
+	return fetch(
+		"http://127.0.0.1:8000/api/v1/comments?parent=true&sort=added:desc"
+	)
+		.then((response) => {
+			console.log(response);
+			if (!response.ok) {
+				LOCAL_DATA.comments = { ...default_state };
+				return default_state;
+			}
+			return response.json().then((data) => {
+				LOCAL_DATA = data;
+				return data;
+			});
+		})
+		.catch((err) => {
+			LOCAL_DATA.comments = { ...default_state };
+			return default_state;
+		});
+};
